3cba39dd3a3140bd67f1a93198a0490f9be644b0
QingChen <liuxinxin@pinglian.hk>
2016-01-21 11:19:15 +0800

    增加通过装饰者模式给被装饰者函数参数动态添加属性的例子，以及简述装饰者模式的缺点

--- app.js
+++ app.js
@@ -24,3 +24,18 @@ duck = Decorator.afterDecorate(duck, function () {
     console.log('鸭子现在可以飞了!');
 });
 duck();
+
+//通过装饰者模式动态给被装饰者函数参数添加属性
+var ajax = function (type, url, param) {
+    console.log('ajax请求的参数为:' + JSON.stringify(param));
+};
+ajax = Decorator.beforeDecorate(ajax, function(type, url, param) {
+    //给ajax的param添加属性
+    param.token = '生成的token';
+});
+ajax('post', 'url', {name:'tom cat'});
+/*
+    打印结果:ajax请求的参数为:{"name":"tom cat","token":"生成的token"}。可知ajax请求自动
+    带上了token参数，而ajax函数依然是个纯洁的ajax函数，如果在其他地方ajax函数不需要token
+    验证的话ajax函数照样可以复用，而不需要对ajax函数进行修改，符合开放-封闭原则
+ */
